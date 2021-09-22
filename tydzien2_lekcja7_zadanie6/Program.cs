fb67a3f74f8f111682a460b1c92930d81285e46e
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-22 11:53:11 +0200

    Add project files.

---
+++ tydzien2_lekcja7_zadanie6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tydzien2_lekcja7_zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 140;
+
+            if (a <= 140)
+               
+            { 
+                Console.WriteLine("Jesteś krasnoludem");
+
+            
+            
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
